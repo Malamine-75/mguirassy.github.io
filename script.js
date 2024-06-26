04bfee8f8811c01d170b17dac69bc8012a05259c
Malamine-75 <malam.guirassy@gmail.com>
2024-06-26 15:23:36 +0200

    Modif portfolio version presque finale

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulaire envoyé avec succès!');
+document.getElementById('enterButton').addEventListener('click', function() {
+    document.getElementById('matrix-animation').style.display = 'block';
+    startMatrixAnimation();
+    
+    setTimeout(function() {
+        // Hide the button and animation container after 5 seconds
+        document.getElementById('enterButton').style.display = 'none';
+        document.getElementById('matrix-animation').style.display = 'none';
+
+        // Show the portfolio content
+        document.querySelector('header').style.display = 'block';
+        document.querySelectorAll('section.content').forEach(section => {
+            section.style.display = 'block';
+        });
+        document.querySelector('footer').style.display = 'block';
+    }, 5000);  // Durée de l'animation en millisecondes
 });
 
-// Animation Matrix
-const canvas = document.getElementById('matrix-canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function startMatrixAnimation() {
+    const canvas = document.getElementById('matrix-canvas');
+    const ctx = canvas.getContext('2d');
 
-const matrixEffect = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%";
     const fontSize = 16;
     const columns = canvas.width / fontSize;
     const drops = Array(Math.floor(columns)).fill(1);
 
-    const draw = () => {
-        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    function draw() {
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "#0f0";
-        ctx.font = `${fontSize}px monospace`;
+
+        ctx.fillStyle = '#0f0';
+        ctx.font = fontSize + 'px monospace';
 
         for (let i = 0; i < drops.length; i++) {
             const text = letters[Math.floor(Math.random() * letters.length)];
@@ -31,15 +45,14 @@ const matrixEffect = () => {
 
             drops[i]++;
         }
-    };
+    }
 
     setInterval(draw, 33);
-};
+}
 
-matrixEffect();
-
+// Réinitialisation de la taille du canvas lors du redimensionnement de la fenêtre
 window.addEventListener('resize', () => {
+    const canvas = document.getElementById('matrix-canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    matrixEffect();
 });
